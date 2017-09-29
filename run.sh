edf1345fca62c0a8bf7b41b5c5dd1897887a2c65
Nobuto Murata <nobuto@ubuntu.com>
2017-09-30 00:51:29 +0900

    more

--- run.sh
+++ run.sh
@@ -5,9 +5,16 @@ set -u
 
 cd "$(dirname "$0")"
 
+lxc profile create quick-maas 2>/dev/null || true
+# 10GB is too small to host VMs
+lxc profile device add quick-maas root disk path=/ pool=default size=50GB 2>/dev/null || true
+
 lxc launch ubuntu:xenial quick-maas \
+    -p default -p quick-maas \
     -c security.privileged=true \
+    -c security.nesting=true \
+    -c raw.lxc='lxc.aa_profile=unconfined' \
     -c user.user-data="$(cat user-script.sh)"
 
-sleep 3
-lxc exec quick-maas -- tail -f /var/log/cloud-init-output.log
+sleep 5
+lxc exec quick-maas -- tail -f -n+1 /var/log/cloud-init-output.log
--- user-script.sh
+++ user-script.sh
@@ -2,13 +2,9 @@
 
 set -e
 set -u
+set -x
 
 export DEBIAN_FRONTEND=noninteractive
-
-# wait online
-#while [ "$(systemctl is-active network-online.target)" != 'active' ]; do
-#    sleep 5
-#done
 
 # proxy
 if host squid-deb-proxy.lxd >/dev/null; then
@@ -16,30 +12,117 @@ if host squid-deb-proxy.lxd >/dev/null; then
     echo "Acquire::http::Proxy \"${http_proxy}\";" > /etc/apt/apt.conf
 fi
 
-# package install
 apt-get update
+
+# KVM setup
+eatmydata apt-get install -y virt-host^
+
+virsh pool-define-as default dir --target /var/lib/libvirt/images
+virsh pool-autostart default
+virsh pool-start default
+
+cat <<EOF | virsh net-define /dev/stdin
+<network>
+  <name>maas</name>
+  <bridge name='maas'/>
+  <ip address='192.168.151.1' netmask='255.255.255.0'/>
+</network>
+EOF
+virsh net-autostart maas
+virsh net-start maas
+
+cat <<EOF | virsh net-define /dev/stdin
+<network>
+  <name>maas-ext</name>
+  <bridge name='maas-ext'/>
+  <ip address='192.168.152.1' netmask='255.255.255.0'/>
+</network>
+EOF
+virsh net-autostart maas-ext
+virsh net-start maas-ext
+
+# maas package install
 eatmydata apt-get install -y maas || true
 
 # bump limit for avahi-daemon
 sed -i -e 's/^rlimit-nproc/#\0/' /etc/avahi/avahi-daemon.conf
 
-# try package install again
+# try maas package install again
 eatmydata apt-get install -y maas
 
-# maas login
+# maas login as ubuntu/ubuntu
 maas createadmin --username ubuntu --password ubuntu \
     --email ubuntu@localhost.localdomain
 
 maas login admin http://localhost/MAAS "$(sudo maas apikey --username ubuntu)"
 
-# import image
+# start importing image
 if [ -n "$http_proxy" ]; then
     maas admin maas set-config name=http_proxy value="$http_proxy"
     maas admin boot-resources import
 fi
+maas admin maas set-config name=completed_intro value=true
 
-echo
-echo 'Waiting for images to be imported...'
+# configure DHCP
+eatmydata apt-get install -y jq
+
+fabric=$(maas admin subnets read | jq -r \
+    '.[] | select(.cidr=="192.168.151.0/24").vlan.fabric')
+maas admin ipranges create type=reserved \
+    start_ip=192.168.151.1 end_ip=192.168.151.100
+maas admin ipranges create type=dynamic \
+    start_ip=192.168.151.201 end_ip=192.168.151.254
+maas admin vlan update "$fabric" 0 dhcp_on=true primary_rack="$HOSTNAME"
+
+fabric=$(maas admin subnets read | jq -r \
+    '.[] | select(.cidr=="192.168.152.0/24").vlan.fabric')
+maas admin ipranges create type=reserved \
+    start_ip=192.168.152.1 end_ip=192.168.152.100
+maas admin ipranges create type=dynamic \
+    start_ip=192.168.152.201 end_ip=192.168.152.254
+maas admin vlan update "$fabric" 0 dhcp_on=true primary_rack="$HOSTNAME"
+
+# wait image
 while [ "$(maas admin boot-resources is-importing)" = 'true' ]; do
-    sleep 5
+    sleep 15
 done
+sleep 30
+
+# MAAS Pod
+sudo -u maas ssh-keygen -f ~maas/.ssh/id_rsa -N ''
+install -m 0600 ~maas/.ssh/id_rsa.pub /root/.ssh/authorized_keys
+
+maas admin pods create \
+    type=virsh \
+    name=localhost \
+    power_address="qemu+ssh://root@localhost/system"
+
+# compose machines
+for i in {1..4}; do
+    maas admin pod compose 1 \
+        cores=2 \
+        memory=2048 \
+        storage='default:8'
+done
+
+# juju
+eatmydata apt-get install -y squashfuse
+snap install --classic juju
+
+juju add-cloud maas /dev/stdin <<EOF
+clouds:
+  maas:
+    type: maas
+    auth-types: [oauth1]
+    endpoint: http://192.168.151.1/MAAS
+EOF
+
+juju add-credential maas -f /dev/stdin <<EOF
+credentials:
+  maas:
+    maas-credential:
+      auth-type: oauth1
+      maas-oauth: $(sudo maas apikey --username ubuntu)
+EOF
+
+juju bootstrap maas maas-controller --debug
