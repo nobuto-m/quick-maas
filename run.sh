cf56091044834085b8392874071280ca0c7fc594
Nobuto Murata <nobuto@ubuntu.com>
2017-10-10 17:24:42 -0400

    Fix virsh start error

--- run.sh
+++ run.sh
@@ -8,12 +8,11 @@ cd "$(dirname "$0")"
 lxc profile create quick-maas 2>/dev/null || true
 # 10GB is too small to host VMs
 lxc profile device add quick-maas root disk path=/ pool=default size=50GB 2>/dev/null || true
+lxc profile device add quick-maas kvm unix-char path=/dev/kvm 2>/dev/null || true
 
 lxc launch ubuntu:xenial quick-maas \
     -p default -p quick-maas \
     -c security.privileged=true \
-    -c security.nesting=true \
-    -c raw.lxc='lxc.aa_profile=unconfined' \
     -c user.user-data="$(cat user-script.sh)"
 
 sleep 5
