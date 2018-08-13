c59301511c932dab4cf5799a729fb2501f859c10
Nobuto Murata <nobuto@ubuntu.com>
2018-08-14 00:15:44 +0900

    allocate 200G

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ cd "$(dirname "$0")"
 
 lxc profile create quick-maas 2>/dev/null || true
 # 10GB is too small to host VMs
-lxc profile device add quick-maas root disk path=/ pool=default size=50GB 2>/dev/null || true
+lxc profile device add quick-maas root disk path=/ pool=default size=200GB 2>/dev/null || true
 lxc profile device add quick-maas kvm unix-char path=/dev/kvm 2>/dev/null || true
 
 lxc launch ubuntu:xenial quick-maas \
