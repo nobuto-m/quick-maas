efd50bb18f41b3dc67631a2bff200a3c7d23b07c
Nobuto Murata <nobuto@ubuntu.com>
2018-10-13 16:38:26 +0900

    wait more for boot kernel to be processed

--- user-script.sh
+++ user-script.sh
@@ -83,7 +83,7 @@ maas admin vlan update "$fabric_id" 0 space=space-first
 while [ "$(maas admin boot-resources is-importing)" = 'true' ]; do
     sleep 15
 done
-sleep 30
+sleep 60
 
 # MAAS Pod
 sudo -u maas ssh-keygen -f ~maas/.ssh/id_rsa -N ''
