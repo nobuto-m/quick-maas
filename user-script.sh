ba7c0c34dad3e0c7a31b6115c5bf2cae319f4382
Nobuto Murata <nobuto@ubuntu.com>
2020-11-26 14:27:14 +0900

    fix typo

--- user-script.sh
+++ user-script.sh
@@ -280,7 +280,7 @@ clouds:
         endpoint: $OS_AUTH_URL
     ca-credentials:
     - |
-$(cat $OS_CERT | sed -e 's/^/      /')
+$(cat $OS_CACERT | sed -e 's/^/      /')
 EOF
 
 cat <<EOF | juju add-credential -c maas-controller --client openstack -f /dev/stdin
