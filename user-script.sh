ab10580c00aa369dff88c1ecbd4fef393e92597e
Nobuto Murata <nobuto@ubuntu.com>
2021-11-18 09:41:03 +0900

    multi networks with "route" mode
    "open" might be better but let's try it with "route" for now

--- user-script.sh
+++ user-script.sh
@@ -57,12 +57,34 @@ cat <<EOF | virsh net-define /dev/stdin
 <network>
   <name>maas</name>
   <bridge name='maas' stp='off'/>
-  <forward mode='nat'/>
+  <forward mode='route'/>
   <ip address='192.168.151.1' netmask='255.255.255.0'/>
 </network>
 EOF
 virsh net-autostart maas
 virsh net-start maas
+
+cat <<EOF | virsh net-define /dev/stdin
+<network>
+  <name>maas2</name>
+  <bridge name='maas2' stp='off'/>
+  <forward mode='route'/>
+  <ip address='192.168.152.1' netmask='255.255.255.0'/>
+</network>
+EOF
+virsh net-autostart maas2
+virsh net-start maas2
+
+cat <<EOF | virsh net-define /dev/stdin
+<network>
+  <name>maas3</name>
+  <bridge name='maas3' stp='off'/>
+  <forward mode='route'/>
+  <ip address='192.168.153.1' netmask='255.255.255.0'/>
+</network>
+EOF
+virsh net-autostart maas3
+virsh net-start maas3
 
 # maas package install
 echo maas-region-controller maas/default-maas-url string 192.168.151.1 \
