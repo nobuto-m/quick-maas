c0a3a1000c89b77a59a4c6c05aa6bc2a782e19dc
Nobuto Murata <nobuto@ubuntu.com>
2023-11-02 10:23:31 +0900

    bug number

--- user-script.sh
+++ user-script.sh
@@ -327,6 +327,7 @@ juju run --format=yaml vault/leader --wait=10m generate-root-ca
 time juju-wait -w --max_wait 1800
 
 # LP: #2040274
+# LP: #2042419
 juju exec --unit ceph-mon/leader '
     ceph osd pool application disable ceph-iscsi unknown --yes-i-really-mean-it
     ceph osd pool application disable iscsi unknown --yes-i-really-mean-it
