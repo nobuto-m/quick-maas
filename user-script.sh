169d602a085c59d6ce20cc8cc14f3afef5ae6b7d
Nobuto Murata <nobuto@ubuntu.com>
2024-02-20 17:08:58 +0900

    explicitly request 22.04 as a workaround

--- user-script.sh
+++ user-script.sh
@@ -385,7 +385,7 @@ juju add-model openstack
 juju model-config num-container-provision-workers=1
 
 # LP: #2039156
-juju deploy -m controller juju-dashboard --to 0
+juju deploy -m controller juju-dashboard --to 0 --base ubuntu@22.04  # LP: #2054375
 juju integrate -m controller controller:dashboard juju-dashboard:controller
 
 juju deploy ./bundle.yaml \
