bd78180f6b0304d4287d8d18ed9cc1ed1ee5f436
Nobuto Murata <nobuto@ubuntu.com>
2024-05-29 23:36:22 +0900

    monitor openstack-machines
    to see if microk8s config can be injected at that point.

--- user-script.sh
+++ user-script.sh
@@ -263,6 +263,9 @@ time sunbeam deployment validate
 time sunbeam cluster bootstrap --manifest manifest.yaml
 
 sunbeam cluster deploy --manifest manifest.yaml &
+    time until juju status -m openstack-machines; do
+        sleep 10
+    done
     time until juju status -m openstack; do
         sleep 10
     done
