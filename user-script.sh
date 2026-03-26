67e4fd7ce54e35bb3db5682a27cb9115582ca144
Nobuto Murata <nobuto@ubuntu.com>
2026-03-26 14:52:05 +0900

    noble image

--- user-script.sh
+++ user-script.sh
@@ -117,12 +117,12 @@ time while [ "$(maas admin boot-resources is-importing)" = 'true' ]; do
     sleep 15
 done
 
-# add jammy image
+# add noble image
 # LP: #2031842
 #sleep 30
 
 # "Boot source selection with this Boot source, Os and Release already exists."
-#maas admin boot-source-selections create 1 os=ubuntu release=jammy arches=amd64 subarches='*' labels='*'
+#maas admin boot-source-selections create 1 os=ubuntu release=noble arches=amd64 subarches='*' labels='*'
 #maas admin boot-resources import
 
 # wait image again
