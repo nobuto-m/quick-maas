4743f9bd8e8482e4b48e2bcdd4f136e2e2d01bdb
Nobuto Murata <nobuto@ubuntu.com>
2026-03-26 17:05:52 +0900

    channel for noble

--- user-script.sh
+++ user-script.sh
@@ -265,7 +265,7 @@ juju add-model openstack
 juju model-defaults num-container-provision-workers=1
 
 # LP: #2039156
-juju deploy -m controller juju-dashboard --to 0 --base ubuntu@24.04  # LP: #2054375
+juju deploy -m controller juju-dashboard --to 0 --channel 0.15/beta --base ubuntu@24.04  # LP: #2054375
 juju integrate -m controller controller:dashboard juju-dashboard:controller
 
 # jammy -> noble
