15d280dc554521321b7aa06e6a35320109cfd942
Nobuto Murata <nobuto@ubuntu.com>
2026-06-17 22:30:01 +0900

    disable the multi space assignment by default

--- user-script.sh
+++ user-script.sh
@@ -223,7 +223,8 @@ vlan_id=$(maas admin vlan read "$fabric_id" 152 | jq -r '.id')
 subnet_id=$(maas admin subnets read | jq -r '.[] | select(.cidr=="192.168.152.0/24").id')
 for system_id in $(maas admin machines read | jq -r '.[].system_id'); do
     maas admin interface update "$system_id" enp3s0 vlan="$vlan_id"
-    maas admin interface link-subnet "$system_id" enp3s0 mode='AUTO' subnet="$subnet_id"
+    # avoid "no obvious space for container "0/lxd/0", host machine has spaces: "space-first", "space-isolated"
+    #maas admin interface link-subnet "$system_id" enp3s0 mode='AUTO' subnet="$subnet_id"
 done
 
 # bootstrap
